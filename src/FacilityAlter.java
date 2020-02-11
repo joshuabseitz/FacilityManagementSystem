b09bb0f0764361ed187ace4320897a35368490df
Joshua Seitz <joshuabseitz@gmail.com>
2020-02-10 18:30:43 -0600

    src for FacilityAlter
    will result in errors because no facility class yet

---
+++ src/FacilityAlter.java
@@ -0,0 +1,11 @@
+public class FacilityAlter {
+  
+  public void addNewFacility(Facility facility) {
+      facility.addNewFacility(facility);
+    }
+
+  public void removeFacility(String id) {
+      facility.remove(id);
+  }
+
+}
