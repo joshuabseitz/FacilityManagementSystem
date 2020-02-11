24e28d141e810f1f84ac009cac465bd18b552354
Joshua Seitz <joshuabseitz@gmail.com>
2020-02-10 18:21:21 -0600

    src for initial facilityInfo
    getters and setters for facilityID, address1, address2, city, state, zip

---
+++ src/facilityInfo.java
@@ -0,0 +1,75 @@
+public class facilityInfo {
+
+    private int FacilityID;
+    private String AddressFieldOne;
+    private String AddressFieldTwo;
+    private String City;
+    private String State;
+    private int ZipCode;
+
+    // set FacilityID
+    public void setFacilityID(int FacilityID){
+      this.FacilityID = FacilityID;
+    }
+    
+    // set AddressFieldOne
+    public void setAddressOne(String AddressFieldOne){
+      this.AddressFieldOne = AddressFieldOne;
+    }
+
+    // set AddressFieldTwo
+    public void setAddressTwo(String AddressFieldTwo){
+      this.AddressFieldTwo = AddressFieldTwo;
+    }
+
+    // set City
+    public void setCity(String City){
+      this.City = City;
+    }
+
+    // set State
+    public void setState(String State){
+      this.State = State;
+    }
+
+    // set Zip Code
+    public void setZipCode(int ZipCode){
+      this.ZipCode = ZipCode;
+    }
+
+    // get FacilityID
+    public int getFacilityID(){
+      return FacilityID;
+    }
+
+    // get AddressFieldOne
+    public String setAddressFieldOne(String AddressFieldOne){
+      return AddressFieldOne;
+    }
+
+    // get AddressFieldTwo
+    public String setAddressFieldTwo(){
+      return AddressFieldTwo;
+    }
+
+    // get AddressFieldTwo
+    public String setAddressTwo(){
+      return AddressFieldTwo;
+    }
+
+    // get City
+    public String setCity(){
+      return City;
+    }
+
+    // get State
+    public String setState(){
+      return State;
+    }
+
+    // get ZipCode
+    public int setZipCode(){
+      return ZipCode;
+    }
+
+}
