af15618d9b870d1af01d68a0683c8e6457bf2439
Joshua Seitz <joshuabseitz@gmail.com>
2020-02-17 17:23:09 -0600

    Create interface_FacilityInfo.java
    interface for facility details

---
+++ src/interface_FacilityInfo.java
@@ -0,0 +1,27 @@
+package com.fms.main;
+
+public interface interface_FacilityInfo {
+
+    public Facility getFacility();
+
+    // Facility ID getter & setters
+    public int getFacilityID();
+    public void setFacilityID(int facilityID);
+
+    // Address getter & setters
+    public String getAddressFieldOne();
+    public void setAddressOne(String AddressFieldOne);
+
+    public String getAddressFieldTwo();
+    public void setAddressTwo(String AddressFieldTwo);
+
+    public String getCity();
+    public void setCity(String city);
+
+    public String getState();
+    public void setState(String state);
+
+    public int getZip();
+    public void setZip(int zip);
+
+}
