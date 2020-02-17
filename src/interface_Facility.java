b5ec82cdd89cb917825e0551f4db8bfc632e8fd3
Joshua Seitz <joshuabseitz@gmail.com>
2020-02-17 23:42:03 +0000

    added public interface for facility

---
+++ src/interface_Facility.java
@@ -0,0 +1,11 @@
+public interface interface_Facility {
+
+    // getter & setter Facility Details
+    public FacilityDetails getFacilityDetail();
+    public void setFacilityDetail(FacilityDetails Details);
+
+    // getter & setter FacilityID
+    public int getFacilityID();
+    public void setFacilityID(int facilityID);
+
+}
