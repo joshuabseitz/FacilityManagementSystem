7430ab20f5e99c4c5e12b827f5f7bfeefae42322
Raitonin <dmartinez11@luc.edu>
2020-02-22 14:03:24 -0600

    Time Model to log problems and maintenance dates

---
+++ src/src/Model/Time.java
@@ -0,0 +1,86 @@
+package src.Model;
+
+public class Time {
+    private int month;
+    private int day;
+    private int year;
+
+    public Time(){}
+
+    public Time(int month, int day, int year)
+    {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public void setDay(int day)
+    {
+        this.day = day;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public boolean olderThan(Time t)
+    {
+        if(this.getYear() < t.getYear())
+        {
+            return true;
+        }
+        else if(this.getYear() > t.getYear())
+        {
+            return false;
+        }
+        else
+        {
+            if(this.getMonth() < t.getMonth())
+            {
+                return true;
+            }
+            else if(this.getMonth() > t.getMonth())
+            {
+                return false;
+            }
+            else
+            {
+                if(this.getDay() < t.getDay())
+                {
+                    return true;
+                }
+                else if(this.getDay() > t.getDay())
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public String toString()
+    {
+        return this.getMonth() + "/" + this.getDay() + "/" + this.getYear();
+    }
+}
+
