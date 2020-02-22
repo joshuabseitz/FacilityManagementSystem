d787a4ad7a21730eec6b96d39cb09b51e6f12fa0
Raitonin <dmartinez11@luc.edu>
2020-02-22 00:00:39 -0600

    Updated Facility DAO and made HashMap which is incorporated in the DAOs

---
+++ src/src/DAL/Database.java
@@ -0,0 +1,9 @@
+package src.DAL;
+
+import src.Model.Facility;
+
+import java.util.HashMap;
+
+public class Database {
+    public static HashMap<Facility, Facility> db = new HashMap<>();
+}
--- src/src/DAL/FacilityDAO.java
+++ src/src/DAL/FacilityDAO.java
@@ -1,5 +1,27 @@
 package src.DAL;
 
+import src.Model.Facility;
+import src.Model.FacilityCapacity;
+import src.Model.FacilityInfo;
+
+import java.util.Collection;
+
 public class FacilityDAO {
     //TODO FacilityDAO
+    public FacilityDAO(){}
+    public Collection<Facility> listFacilities()
+    {
+        return Database.db.values();
+    }
+
+    public FacilityInfo getFacilityInfo(Facility f)
+    {
+        return Database.db.get(f).getFacilityInfo();
+    }
+
+    public FacilityCapacity getFacilityCapacity(Facility f)
+    {
+        return Database.db.get(f).getFacilityCapacity();
+    }
+
 }
