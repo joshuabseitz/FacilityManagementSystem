c394cc4ce0b4ac093d6117fc2344d05e33d5dc57
Raitonin <dmartinez11@luc.edu>
2020-02-23 00:35:58 -0600

    MaintenanceOrder model

---
+++ src/src/Model/MaintenanceOrder.java
@@ -0,0 +1,70 @@
+package src.Model;
+
+public class MaintenanceOrder {
+    private int priority;
+    private String detail;
+    private Date requestDate;
+    private Date orderDate;
+    private int cost;
+
+    public MaintenanceOrder(){}
+
+    public MaintenanceOrder(MaintenanceRequest MaintReq, Date orderDate, int cost)
+    {
+        this.priority = MaintReq.getPriority();
+        this.detail = MaintReq.getDetail();
+        this.requestDate = MaintReq.getRequestDate();
+        this.orderDate = orderDate;
+        this.cost = cost;
+    }
+
+    public int getPriority()
+    {
+        return priority;
+    }
+
+    public void setPriority(int priority)
+    {
+        this.priority = priority;
+    }
+
+    public String getDetail()
+    {
+        return detail;
+    }
+
+    public void setDetail(String detail)
+    {
+        this.detail = detail;
+    }
+
+    public Date getRequestDate()
+    {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate)
+    {
+        this.requestDate = requestDate;
+    }
+
+    public Date getOrderDate()
+    {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate)
+    {
+        this.orderDate = orderDate;
+    }
+
+    public int getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(int cost)
+    {
+        this.cost = cost;
+    }
+}
