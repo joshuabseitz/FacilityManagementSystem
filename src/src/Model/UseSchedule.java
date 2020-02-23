89e4650a3de21b43dfd1eec1be31bb64c93c4157
Raitonin <dmartinez11@luc.edu>
2020-02-23 00:23:29 -0600

    UseSchedule Model

---
+++ src/src/Model/UseSchedule.java
@@ -0,0 +1,22 @@
+package src.Model;
+
+import java.util.HashMap;
+
+public class UseSchedule {
+    private HashMap<UseRequest, Interval> schedule;
+
+    UseSchedule()
+    {
+        schedule = new HashMap<UseRequest, Interval>();
+    }
+
+    public HashMap<UseRequest, Interval> getSchedule()
+    {
+        return schedule;
+    }
+
+    public void setSchedule(HashMap<UseRequest, Interval> schedule)
+    {
+        this.schedule = schedule;
+    }
+}
