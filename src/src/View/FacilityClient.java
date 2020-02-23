8f99156a7a7297addc08006580986a0798e2da17
Joshua Seitz <joshuabseitz@gmail.com>
2020-02-22 21:43:18 -0600

    Update FacilityClient.java
    progress - errors present in obj creation, still working

--- src/src/View/FacilityClient.java
+++ src/src/View/FacilityClient.java
@@ -1,8 +1,26 @@
 package src.View;
-import java.util.List;
+
+// import java.util.List;
+import src.Model.FacilityInfo;
+import src.Model.interface_FacilityInfo;
+
 
 public class FacilityClient {
-    //TODO FacilityClient
-    //TODO Maintenance
+
+    public FacilityClient() throws Exception {
+
+        //office1
+        interface_FacilityInfo office1 = new FacilityInfo(); //No object creation
+        interface_FacilityInfo office1Details = new FacilityInfo();
+        office1.setFacilityID(100);
+        office1Details.setAddressOne("6330 N Sheridan Rd");
+        office1Details.setAddressTwo("Suit 03");
+        office1Details.setCity("Chicago");
+        office1Details.setState("Illinois");
+        office1Details.setZip(60660);
+
+        // office1.setFacilityInfo(office1Details);
+
+    }
 
 }
