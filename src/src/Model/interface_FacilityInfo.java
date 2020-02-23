82910a01545232bbded6009e417e0389827e16fc
Raitonin <dmartinez11@luc.edu>
2020-02-23 01:00:56 -0600

    Updated interface_FacilityInfo

--- src/src/Model/interface_FacilityInfo.java
+++ src/src/Model/interface_FacilityInfo.java
@@ -2,26 +2,23 @@ package src.Model;
 
 public interface interface_FacilityInfo {
 
-    public Facility getFacility();
+    Facility getFacility();
 
     // src.Model.Facility ID getter & setters
-    public int getFacilityID();
-    public void setFacilityID(int facilityID);
+    int getFacilityID();
+    void setFacilityID(int facilityID);
 
     // Address getter & setters
-    public String getAddressFieldOne();
-    public void setAddressOne(String AddressFieldOne);
+    String getAddressField();
+    void setAddress(String AddressField);
 
-    public String getAddressFieldTwo();
-    public void setAddressTwo(String AddressFieldTwo);
+    String getCity();
+    void setCity(String city);
 
-    public String getCity();
-    public void setCity(String city);
+    String getState();
+    void setState(String state);
 
-    public String getState();
-    public void setState(String state);
-
-    public int getZip();
-    public void setZip(int zip);
+    int getZip();
+    void setZip(int zip);
 
 }
