7ee5afea2bd13301aa54cbd8571a4d79182e22ed
Raitonin <dmartinez11@luc.edu>
2020-02-23 01:51:19 -0600

    Update FacilityCapacity

--- src/src/Model/FacilityCapacity.java
+++ src/src/Model/FacilityCapacity.java
@@ -2,6 +2,45 @@ package src.Model;
 
 public class FacilityCapacity {
     private int facilityCapacity;
-    public int getFacilityCapacity(){ return facilityCapacity; }
-    public void setFacilityCapacity(int facilityCapacity){this.facilityCapacity = facilityCapacity;}
+    private String name;
+    private int squareFeet;
+
+    FacilityCapacity(){}
+
+    public FacilityCapacity(String name, int facilityCapacity, int squareFeet)
+    {
+        this.name = name;
+        this.facilityCapacity = facilityCapacity;
+        this.squareFeet = squareFeet;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getFacilityCapacity()
+    {
+        return facilityCapacity;
+    }
+
+    public void setFacilityCapacity(int facilityCapacity)
+    {
+        this.facilityCapacity = facilityCapacity;
+    }
+
+    public int getSquareFeet()
+    {
+        return squareFeet;
+    }
+
+    public void setSquareFeet(int squareFeet)
+    {
+        this.squareFeet = squareFeet;
+    }
 }
