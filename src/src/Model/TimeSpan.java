e452c04786e5041b127d24ddac07ca14db66ec73
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:41:45 -0500

    Update TimeSpan.java

--- src/src/Model/TimeSpan.java
+++ src/src/Model/TimeSpan.java
@@ -3,22 +3,25 @@ package src.Model;
 public class TimeSpan {
     private int timeSpan;
 
+    //setter for timespan
     public TimeSpan(int timeSpan)
     {
         this.timeSpan = timeSpan;
     }
 
+    //setter fopr timespan
     public TimeSpan(Time startTime, Time endTime)
     {
         this.timeSpan = (endTime.getHour() - startTime.getHour())*60 + (endTime.getMinute() - startTime.getMinute());
     }
 
-    //returns timeSpan in minutes
+    //getter for timespan
     public int getTimeSpan()
     {
         return timeSpan;
     }
 
+    //setter for timespan
     public void setTimeSpan(int timeSpan)
     {
         this.timeSpan = timeSpan;
