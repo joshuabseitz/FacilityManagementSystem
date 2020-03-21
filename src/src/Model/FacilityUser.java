b0ce8181797197c999bfe455b8b6d1f0c10dfe12
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:16:02 -0500

    Added class comments

--- src/src/Model/FacilityUser.java
+++ src/src/Model/FacilityUser.java
@@ -1,12 +1,14 @@
 package src.Model;
 
 public class FacilityUser {
+    //instantiates variables
     private String userID;
     private String userName;
     private long userPhone;
     private String userEmail;
     private String userRole;
 
+    //method for setting variables
     public FacilityUser(String userID, String userName, long userPhone, String userEmail, String userRole)
     {
         this.userID = userID;
@@ -16,51 +18,61 @@ public class FacilityUser {
         this.userRole = userRole;
     }
 
+    //getter for user id
     public String getUserID()
     {
         return userID;
     }
 
+    //setter for user id
     public void setUserID(String userID)
     {
         this.userID = userID;
     }
 
+    //getter for user name
     public String getUserName()
     {
         return userName;
     }
 
+    //setter for user name
     public void setUserName(String userName)
     {
         this.userName = userName;
     }
 
+    //getter for user phone
     public long getUserPhone()
     {
         return userPhone;
     }
 
+    //setter for user phone
     public void setUserPhone(long userPhone)
     {
         this.userPhone = userPhone;
     }
 
+    //getter for user email
     public String getUserEmail()
     {
         return userEmail;
     }
 
+    //setter for user email
     public void setUserEmail(String userEmail)
     {
         this.userEmail = userEmail;
     }
 
+    //getter for user role
     public String getUserRole()
     {
         return userRole;
     }
 
+    //setter for user role
     public void setUserRole(String userRole)
     {
         this.userRole = userRole;
