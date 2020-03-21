5755a866468e87a44eb8bd0bbbc5ae27007edb33
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:07:27 -0500

    Added class comments

--- src/src/Model/Date.java
+++ src/src/Model/Date.java
@@ -1,10 +1,12 @@
 package src.Model;
 
 public class Date {
+    //instantiates variables
     private int month;
     private int day;
     private int year;
-
+    
+    //setter method for month, day, and year
     public Date(int month, int day, int year)
     {
         this.month = month;
@@ -14,36 +16,43 @@ public class Date {
 
     public Date() {}
 
+    //getter for month variable
     public int getMonth()
     {
         return month;
     }
 
+    //setter for month variable
     public void setMonth(int month)
     {
         this.month = month;
     }
 
+    //getter for day variable
     public int getDay()
     {
         return day;
     }
 
+    //setter for day variable
     public void setDay(int day)
     {
         this.day = day;
     }
 
+    //getter for year variable
     public int getYear()
     {
         return year;
     }
 
+    //setter for year variable
     public void setYear(int year)
     {
         this.year = year;
     }
 
+    //checks given date with set date
     public boolean olderThan(Date t)
     {
         if(this.getYear() < t.getYear())
@@ -78,6 +87,8 @@ public class Date {
         }
         return true;
     }
+    
+    //returns date
     public String toString()
     {
         return this.getMonth() + "/" + this.getDay() + "/" + this.getYear();
