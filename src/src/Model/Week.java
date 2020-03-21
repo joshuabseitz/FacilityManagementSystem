4536504cd5f711c28c258b3b217b84b4dee0b377
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:46:30 -0500

    Added class comments

--- src/src/Model/Week.java
+++ src/src/Model/Week.java
@@ -1,6 +1,7 @@
 package src.Model;
 
 public class Week {
+    //instantiates variables
     private boolean Mon;
     private boolean Tue;
     private boolean Wed;
@@ -9,6 +10,7 @@ public class Week {
     private boolean Sat;
     private boolean Sun;
 
+    //method for setting variables
     public Week(boolean Mon, boolean Tue, boolean Wed, boolean Thurs, boolean Fri, boolean Sat, boolean Sun)
     {
         this.Mon = Mon;
@@ -20,76 +22,91 @@ public class Week {
         this.Sun = Sun;
     }
 
+    //checks for monday
     public boolean isMon()
     {
         return Mon;
     }
 
+    //setter for monday
     public void setMon(boolean Mon)
     {
         this.Mon = Mon;
     }
 
+    //checks for tuesday
     public boolean isTue()
     {
         return Tue;
     }
 
+    //setter for tuesday
     public void setTue(boolean Tue)
     {
         this.Tue = Tue;
     }
 
+    //checks for wednesday
     public boolean isWed()
     {
         return Wed;
     }
 
+    //setter for wednesday
     public void setWed(boolean Wed)
     {
         this.Wed = Wed;
     }
 
+    //checks for thursday
     public boolean isThurs()
     {
         return Thurs;
     }
 
+    //setter for thursday
     public void setThurs(boolean Thurs)
     {
         this.Thurs = Thurs;
     }
 
+    //checks for friday
     public boolean isFri()
     {
         return Fri;
     }
 
+    //setter for friday
     public void setFri(boolean Fri)
     {
         this.Fri = Fri;
     }
 
+    //checks for saturday
     public boolean isSat()
     {
         return Sat;
     }
 
+    //setter for saturday
     public void setSat(boolean Sat)
     {
         this.Sat = Sat;
     }
 
+    //checks for sunday
     public boolean isSun()
     {
         return Sun;
     }
 
+    //setter for sunday
     public void setSun(boolean Sun)
     {
         this.Sun = Sun;
     }
 
+    //checks that week is cumulative
     public boolean cumulative(Week w)
     {
         if(this.isMon() == w.isMon() || this.isTue() == w.isTue() || this.isWed() == w.isWed() || this.isThurs() == w.isThurs() || this.isFri() == w.isFri() || this.isSat() == w.isSat() || this.isSun() == w.isSun())
@@ -99,6 +116,7 @@ public class Week {
         return false;
     }
 
+    //returns total number of days a facility is in use
     public int daysInUse()
     {
         int totalDays = 0;
