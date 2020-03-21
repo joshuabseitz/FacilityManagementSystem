834179fc4ad4bc8f9a0addb04d5dedb2298019ce
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:37:17 -0500

    Added class comments

--- src/src/Model/MaintenanceSchedule.java
+++ src/src/Model/MaintenanceSchedule.java
@@ -3,6 +3,7 @@ package src.Model;
 import java.util.HashMap;
 
 public class MaintenanceSchedule {
+    //instantiates schedule
     private HashMap<MaintenanceOrder, Interval> schedule;
 
     public MaintenanceSchedule()
@@ -10,11 +11,13 @@ public class MaintenanceSchedule {
         schedule = new HashMap<MaintenanceOrder, Interval>();
     }
 
+    //getter for maintenance schedule
     public HashMap<MaintenanceOrder, Interval> getSchedule()
     {
         return schedule;
     }
 
+    //setter for maintenance schedule
     public void setSchedule(HashMap<MaintenanceOrder, Interval> schedule)
     {
         this.schedule = schedule;
