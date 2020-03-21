9c45576b8da0f64be657b74b1df8ccb5aec6a7dd
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:00:17 -0500

    Added class comments

--- src/src/DAL/UseDAO.java
+++ src/src/DAL/UseDAO.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class UseDAO {
     public UseDAO(){}
 
+    //checks to see if a specified facility is in use during a specific date and time
     public boolean inUseDuringInterval(Facility f, Date d, Time start, Time end)
     {
         Interval comp = new Interval(d, start, end);
@@ -19,7 +20,8 @@ public class UseDAO {
         }
         return false;
     }
-
+    
+    //checks to see if a specified facility is in use during a specific week and time
     public boolean inUseDuringInterval(Facility f, Week w, Time start, Time end)
     {
         Interval comp = new Interval(w, start, end);
@@ -32,27 +34,32 @@ public class UseDAO {
         }
         return false;
     }
-
+    
+    //assigns specified facility in use for a specified request
     public void assignFacilityToUse(Facility f, UseRequest useRequest)
     {
         Database.db.get(f).getFacilityUse().getSchedule().getSchedule().put(useRequest, useRequest.getIntervalSlot());
     }
 
+    //vacates specified facility
     public void vacateFacility(Facility f)
     {
         Database.db.get(f).getFacilityUse().getSchedule().getSchedule().clear();
     }
 
+    //lists inspections of specified facility
     public ArrayList<Inspection> listInspections(Facility f)
     {
         return Database.db.get(f).getInspections();
     }
 
+    //lists the schedule of specified facility
     public UseSchedule listActualUsage (Facility f)
     {
         return Database.db.get(f).getFacilityUse().getSchedule();
     }
 
+    //returns how long a specified facility is in use for
     public double calcUsageRate(Facility f)
     {
         float totalMinutesInUse = 0;
