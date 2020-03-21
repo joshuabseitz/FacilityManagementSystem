42cf2df465385e6bd81cb675b966ac9bd543f9df
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:09:41 -0500

    Added class comments

--- src/src/Model/FacilityCapacity.java
+++ src/src/Model/FacilityCapacity.java
@@ -1,42 +1,50 @@
 package src.Model;
 
 public class FacilityCapacity{
+    //instantiates variables
     private String name;
     private int facilityCapacity;
     private int squareFeet;
 
+    //method for setting variables
     public FacilityCapacity(String name, int facilityCapacity, int squareFeet)
     {
         this.name = name;
         this.facilityCapacity = facilityCapacity;
         this.squareFeet = squareFeet;
     }
-
+    
+    //getter for facility name
     public String getName()
     {
         return name;
     }
 
+    //setter for facility name
     public void setName(String name)
     {
         this.name = name;
     }
 
+    //getter for facility capacity
     public int getFacilityCapacity()
     {
         return facilityCapacity;
     }
 
+    //setter for facility capacity
     public void setFacilityCapacity(int facilityCapacity)
     {
         this.facilityCapacity = facilityCapacity;
     }
 
+    //getter for square feet of facility
     public int getSquareFeet()
     {
         return squareFeet;
     }
 
+    //setter for square feet of facility
     public void setSquareFeet(int squareFeet)
     {
         this.squareFeet = squareFeet;
