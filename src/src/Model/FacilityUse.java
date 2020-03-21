43516a7f1845663fdc3668432474e1dd60cf7f58
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:14:13 -0500

    Added class comments

--- src/src/Model/FacilityUse.java
+++ src/src/Model/FacilityUse.java
@@ -3,30 +3,36 @@ package src.Model;
 import java.util.ArrayList;
 
 public class FacilityUse {
+    //instantiates variable
     private ArrayList<UseRequest> requests;
     private UseSchedule schedule;
 
+    //method for setting variables
     public FacilityUse(UseSchedule schedule)
     {
         this.requests = new ArrayList<UseRequest>();
         this.schedule = schedule;
     }
 
+    //getter for facility requests
     public ArrayList<UseRequest> getRequests()
     {
         return requests;
     }
 
+    //adds facility request
     public void addRequest(UseRequest request)
     {
         requests.add(request);
     }
 
+    //getter for facility schedule
     public UseSchedule getSchedule()
     {
         return schedule;
     }
 
+    //setter for facility schedule
     public void setSchedule(UseSchedule schedule)
     {
         this.schedule = schedule;
