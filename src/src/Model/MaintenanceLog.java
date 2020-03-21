02faff9bf8337f05f6b69703cd91cbcab65574ac
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:24:15 -0500

    Added class comments

--- src/src/Model/MaintenanceLog.java
+++ src/src/Model/MaintenanceLog.java
@@ -3,6 +3,7 @@ package src.Model;
 import java.util.HashMap;
 
 public class MaintenanceLog {
+    //instantiates log
     private HashMap<MaintenanceOrder, Interval> log;
 
     public MaintenanceLog()
@@ -10,11 +11,13 @@ public class MaintenanceLog {
         log = new HashMap<MaintenanceOrder, Interval>();
     }
 
+    //getter for maintenance log
     public HashMap<MaintenanceOrder, Interval> getLog()
     {
         return log;
     }
 
+    //setter for maintenance log
     public void setLog(HashMap<MaintenanceOrder, Interval> log)
     {
         this.log = log;
