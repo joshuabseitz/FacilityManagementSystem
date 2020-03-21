f58f7073d10a1fc36074be1f1e03957aac7a7173
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:43:56 -0500

    Added class comments

--- src/src/Model/UseSchedule.java
+++ src/src/Model/UseSchedule.java
@@ -3,6 +3,7 @@ package src.Model;
 import java.util.HashMap;
 
 public class UseSchedule {
+    //instantiates schedule
     private HashMap<UseRequest, Interval> schedule;
 
     public UseSchedule()
@@ -10,11 +11,13 @@ public class UseSchedule {
         schedule = new HashMap<UseRequest, Interval>();
     }
 
+    //getter for facility use schedule
     public HashMap<UseRequest, Interval> getSchedule()
     {
         return schedule;
     }
 
+    //setter for facility use schedule
     public void setSchedule(HashMap<UseRequest, Interval> schedule)
     {
         this.schedule = schedule;
