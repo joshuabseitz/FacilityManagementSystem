fdbd4116096974f4b482cb52fa7e5545c5375629
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:43:10 -0500

    Added class comments

--- src/src/Model/UseRequest.java
+++ src/src/Model/UseRequest.java
@@ -1,30 +1,36 @@
 package src.Model;
 
 public class UseRequest {
+    //instantiates variables
     private FacilityUser user;
     private Interval intervalSlot;
 
+    //method for setting variables
     public UseRequest(FacilityUser user, Interval intervalSlot)
     {
         this.user = user;
         this.intervalSlot = intervalSlot;
     }
 
+    //getter for facility user
     public FacilityUser getUser()
     {
         return user;
     }
 
+    //setter for facility user
     public void setUser(FacilityUser user)
     {
         this.user = user;
     }
 
+    //getter for facility interval slot
     public Interval getIntervalSlot()
     {
         return intervalSlot;
     }
 
+    //setter for facility interval slot
     public void setIntervalSlot(Interval intervalSlot)
     {
         this.intervalSlot = intervalSlot;
