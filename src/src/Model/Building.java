7db140b60ca31a63b6f04e1c614531204ea2c9bb
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:04:39 -0500

    Added class comments

--- src/src/Model/Building.java
+++ src/src/Model/Building.java
@@ -3,12 +3,14 @@ package src.Model;
 import java.util.ArrayList;
 
 public class Building implements Facility {
+    //instantiate variables
     private FacilityInfo facilityInfo;
     private FacilityCapacity facilityCapacity;
     private FacilityUse use;
     private FacilityMaintenance maintenance;
     private ArrayList<Inspection> inspections;
-
+    
+    //creates setters for the instantiated variables
     public Building(FacilityInfo facilityInfo, FacilityCapacity facilityCapacity, FacilityUse use, FacilityMaintenance maintenance)
     {
         this.facilityInfo = facilityInfo;
@@ -17,52 +19,62 @@ public class Building implements Facility {
         this.maintenance = maintenance;
         this.inspections = new ArrayList<Inspection>();
     }
-
+    
+    //getter for facilityinfo
     public FacilityInfo getFacilityInfo()
     {
         return facilityInfo;
     }
 
+    //setter for facilityinfo
     public void setFacilityInfo(FacilityInfo facilityInfo)
     {
         this.facilityInfo = facilityInfo;
     }
 
+    //getter for facilitycapacity
     public FacilityCapacity getFacilityCapacity()
     {
         return facilityCapacity;
     }
 
+    //setter for facilitycapacity
     public void setFacilityCapacity(FacilityCapacity facilityCapacity)
     {
         this.facilityCapacity = facilityCapacity;
     }
 
+    //getter for facilityuse
     public FacilityUse getFacilityUse()
     {
         return use;
     }
 
+    //setter for facilityuse
     public void setFacilityUse(FacilityUse use)
     {
         this.use = use;
     }
 
+    //getter for facilitymaintenance
     public FacilityMaintenance getFacilityMaintenance()
     {
         return maintenance;
     }
 
+    //setter for facilitymaintenance
     public void setFacilityMaintenance(FacilityMaintenance maintenance)
     {
         this.maintenance = maintenance;
     }
 
+    //getter for facility inspections
     public ArrayList<Inspection> getInspections()
     {
         return inspections;
     }
 
+    //adds inspections to arraylist
     public void addInspection(Inspection inspection)
     {
         inspections.add(inspection);
