9fa20386b19ad1d7e9e5bc96eb4b82bd2238518a
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:13:02 -0500

    Added class comments

--- src/src/Model/FacilityMaintenance.java
+++ src/src/Model/FacilityMaintenance.java
@@ -3,11 +3,13 @@ package src.Model;
 import java.util.ArrayList;
 
 public class FacilityMaintenance {
+    //instantiates variables
     private MaintenanceSchedule maintenanceSchedule;
     private ArrayList<MaintenanceOrder> maintenanceOrders;
     private MaintenanceLog maintenanceLog;
     private ArrayList<MaintenanceRequest> maintenanceRequests;
 
+    //method for setting variables
     public FacilityMaintenance(MaintenanceSchedule schedule, MaintenanceLog log)
     {
         this.maintenanceSchedule = schedule;
@@ -16,47 +18,56 @@ public class FacilityMaintenance {
         this.maintenanceRequests = new ArrayList<MaintenanceRequest>();
     }
 
+    //getter for maintenance schedule
     public MaintenanceSchedule getMaintenanceSchedule()
     {
         return maintenanceSchedule;
     }
 
+    //setter for maintenance schedule
     public void setMaintenanceSchedule(MaintenanceSchedule maintenanceSchedule)
     {
         this.maintenanceSchedule = maintenanceSchedule;
     }
 
+    //getter for maintenance orders
     public ArrayList<MaintenanceOrder> getMaintenanceOrders()
     {
         return maintenanceOrders;
     }
 
+    //getter for maintenance log
     public MaintenanceLog getMaintenanceLog()
     {
         return maintenanceLog;
     }
 
+    //getter for maintenance requests
     public ArrayList<MaintenanceRequest> getMaintenanceRequests()
     {
         return maintenanceRequests;
     }
 
+    //adds maintenance requests 
     public void addMaintenanceRequest(MaintenanceRequest maintenanceRequest)
     {
         maintenanceRequests.add(maintenanceRequest);
     }
 
+    //adds maintenance order to schedule
     public void addOrderToSchedule(MaintenanceOrder maintenanceOrder, Interval intervalSlot)
     {
         maintenanceSchedule.getSchedule().put(maintenanceOrder, intervalSlot);
         maintenanceOrders.remove(maintenanceOrder);
     }
 
+    //adds maintenance order
     public void addMaintenanceOrder(MaintenanceOrder maintenanceOrder)
     {
         maintenanceOrders.add(maintenanceOrder);
     }
 
+    //adds maintenance order to maintenance log
     public void addOrderToLog(MaintenanceOrder maintenanceOrder)
     {
         maintenanceLog.getLog().put(maintenanceOrder, maintenanceSchedule.getSchedule().get(maintenanceOrder));
