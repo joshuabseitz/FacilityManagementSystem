5a42c458a18a8ace35e08d731f5a9e6679a0b607
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:27:55 -0500

    Added class comments

--- src/src/Model/MaintenanceRequest.java
+++ src/src/Model/MaintenanceRequest.java
@@ -1,10 +1,12 @@
 package src.Model;
 
 public class MaintenanceRequest {
+    //instantiates variables
     private int priority;
     private String detail;
     private Date requestDate;
 
+    //method for setting variables
     public MaintenanceRequest(int priority, String detail, Date requestDate)
     {
         this.priority = priority;
@@ -12,31 +14,37 @@ public class MaintenanceRequest {
         this.requestDate = requestDate;
     }
 
+    //getter for maintenance priority
     public int getPriority()
     {
         return priority;
     }
 
+    //setter for maintenance priority
     public void setPriority(int priority)
     {
         this.priority = priority;
     }
 
+    //getter for maintenance detail
     public String getDetail()
     {
         return detail;
     }
 
+    //setter for maintenance detail
     public void setDetail(String detail)
     {
         this.detail = detail;
     }
 
+    //getter for maintenance request date
     public Date getRequestDate()
     {
         return requestDate;
     }
 
+    //setter for maintenance request date
     public void setRequestDate(Date requestDate)
     {
         this.requestDate = requestDate;
