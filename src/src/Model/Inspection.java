23703f3593e15dd78fa34f3f77ad1890e38711d8
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 22:17:28 -0500

    Added class comments

--- src/src/Model/Inspection.java
+++ src/src/Model/Inspection.java
@@ -1,30 +1,36 @@
 package src.Model;
 
 public class Inspection {
+    //instantiates variables
     private String inspectionType;
     private Interval interval;
 
+    //method for setting variables
     public Inspection(String inspectionType, Interval interval)
     {
         this.inspectionType = inspectionType;
         this.interval = interval;
     }
 
+    //getter for facility inspection type
     public String getInspectionType()
     {
         return inspectionType;
     }
 
+    //setter for facility inspection type
     public void setInspectionType(String inspectionType)
     {
         this.inspectionType = inspectionType;
     }
 
+    //getter for inspection interval
     public Interval getInterval()
     {
         return interval;
     }
 
+    //setter for inspection interval
     public void setInterval(Interval interval)
     {
         this.interval = interval;
