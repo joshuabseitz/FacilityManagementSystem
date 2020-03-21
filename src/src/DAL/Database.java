959f3620d0fa77c601a2259d2eb3800746a3f0ca
dmartinez8198 <dmartinez11@luc.edu>
2020-03-20 19:47:35 -0500

    Added class comments

--- src/src/DAL/Database.java
+++ src/src/DAL/Database.java
@@ -5,5 +5,6 @@ import src.Model.Facility;
 import java.util.HashMap;
 
 public class Database {
+    // creates HashMap to store the data from one class to another
     public static HashMap<Facility, Facility> db = new HashMap<>();
 }
